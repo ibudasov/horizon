d5f6998a502af8e871a15848c699fb99f15e709c
Igor <igor.budasov@cm.com>
2022-09-28 18:43:19 +0200

    graphql mutation for templates

--- src/GraphQL/Resolver/TemplateResolverMap.php
+++ src/GraphQL/Resolver/TemplateResolverMap.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\GraphQL\Resolver;
 
 use App\Core\Component\Template\Application\Repository\Database\TemplateRepository;
+use App\Core\Component\Template\Domain\Template;
+use App\Core\Component\Template\Domain\TemplateName;
 use Overblog\GraphQLBundle\Resolver\ResolverMap;
 use ArrayObject;
 use GraphQL\Type\Definition\ResolveInfo;
@@ -30,6 +32,19 @@ class TemplateResolverMap extends ResolverMap
                 ) {
                     if ($info->fieldName === 'templates') {
                         return $this->repository->allTemplates();
+                    }
+                    return null;
+                },
+            ],
+            'RootMutation' => [
+                self::RESOLVE_FIELD => function (
+                    $value,
+                    ArgumentInterface $args,
+                    ArrayObject $context,
+                    ResolveInfo $info
+                ) {
+                    if ($info->fieldName === 'createTemplate') {
+                        return new Template(new TemplateName('use repo here!'));
                     }
                     return null;
                 },
